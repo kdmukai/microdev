36dc68c5ff036af9362562ac82cc175d6fc4437f
Keith Mukai <keith.mukai@essaytagger.com>
2012-09-03 15:26:20 -0500

    Initial commit

---
+++ utils.py
@@ -0,0 +1,10 @@
+
+def render_template(template_name, context_dict):
+    from django.template.loader import get_template
+    from django.template import Context
+
+    # User Django's template system to render the template
+    context = Context(context_dict)
+    
+    # Render and return as a string...
+    return get_template(template_name).render(context)
