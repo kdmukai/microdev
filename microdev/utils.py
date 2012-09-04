b7836edee009cfab5ead74a3bbc720b3d36c909e
Keith Mukai <keith.mukai@essaytagger.com>
2012-09-04 16:50:10 -0500

    Comment typo fix.

--- microdev/utils.py
+++ microdev/utils.py
@@ -6,7 +6,7 @@ def render_template(template_name, context_dict):
     from django.template.loader import get_template
     from django.template import Context
 
-    # User Django's template system to render the template
+    # Use Django's template system to render the template
     context = Context(context_dict)
     
     # Render and return as a string...
