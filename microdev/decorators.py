5de1516f2b96bd67df8ff915b31322901b920309
Keith Mukai <keith.mukai@essaytagger.com>
2012-09-05 14:35:02 -0500

    Removed print statements and clarified comments.

--- microdev/decorators.py
+++ microdev/decorators.py
@@ -9,27 +9,31 @@ from microdev import POST_LOGIN_REDIR_PARAM
 
 """--------------------------------------------------------------------------
     Stores the current url in the session so that it can be retrieved after a 
-    third-party oauth login (e.g. login with Facebook). Then calls the normal 
-    login_required decorator.
+    third-party oauth login (e.g. login with Facebook). In these cases the 
+    normal 'next' request param is lost in the oauth exchange whereas the
+    session is persistent when the user returns from the oauth intermediate 
+    flow.
+    
+    Decorator then calls the standard login_required decorator.
     
     km: I'll be honest; I barely understand decorators and function wrapping.
     It's working as expected but only because of a ton of trial-and-error.
 --------------------------------------------------------------------------"""
 def login_required_with_session_redirect(function=None, redirect_field_name=None, login_url=None):
-    print('login_required_with_session_redirect')
 
     def add_redir_param(function):
         @wraps(function)
         def wrapper(request, *args, **kwargs):
-            print('add_redir_param')
+            # Store the current URL in the session
             request.session[POST_LOGIN_REDIR_PARAM] = request.path
             
-            print('set post_login_redir_url: ' + request.session[POST_LOGIN_REDIR_PARAM])
+            # We're done here. Let the incoming function execute
             return function(request, *args, **kwargs)
         return wrapper
 
-
+    # Call the decorator's method and then relinquish control to login_required
     if function:
         return add_redir_param(login_required(function))
     else:
+        # km: When would function be None?
         return add_redir_param(login_required)
