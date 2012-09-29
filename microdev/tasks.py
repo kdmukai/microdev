2887781fb3266dc646390d218d515733b0641e2b
Keith Mukai <keith.mukai@essaytagger.com>
2012-09-29 14:05:40 -0500

    Now defaults to HTML mail.

--- microdev/tasks.py
+++ microdev/tasks.py
@@ -2,10 +2,12 @@
 
 
 #@task(name="microdev.tasks.send_email")
-def send_email(subject, body, from_address, to_address):
+def send_email(subject, body, from_address, to_address, as_html=True):
     from django.core.mail import EmailMessage
     
     msg = EmailMessage(subject, body, from_address, [to_address])
+    if as_html:
+        msg.content_subtype = "html"  # Main content is now text/html
     msg.send(True)
 
     return 1
