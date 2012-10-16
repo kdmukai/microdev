edc9b5d7ef9e9d8e599a23fe61c32cf202792a1e
Keith Mukai <keith.mukai@essaytagger.com>
2012-10-16 18:00:18 -0500

    - Added ChangeLoggerMixin documentation. - Further refinements to ChangeLoggerMixin integration.

--- README.md
+++ README.md
@@ -4,3 +4,80 @@ microdev
 A collection of reusable Django utility modules for common methods.
 
 This is mostly for my own uses as I build more projects in Django. Whenever I create a new method that I'll probably need to re-use later, I'll add it here.
+
+
+## ChangeLoggerMixin ##
+The microdev.models.ChangeLoggerMixin provides per-field change logging for any Django model. Here's a simple Django model:
+
+```python
+class MyModel(models.Model):
+	favorite_color = models.CharField(max_length=128)
+	favorite_number = models.IntegerField()
+	date_updated = models.DateTimeField()
+```
+
+Next we add the ChangeLoggerMixin functionality and define a ChangeLog implementation class:
+
+```python
+from microdev.models import ChangeLoggerMixin
+from microdev.models import ChangeLog
+	
+class MyModelChangeLog(ChangeLog):
+	pass
+
+class MyModel(models.Model, ChangeLoggerMixin):
+	favorite_color = models.CharField(max_length=128)
+	favorite_number = models.IntegerField()
+	
+    # Overrides from ChangeLoggerMixin
+    _change_logger_mixin__change_log_class = MyModelChangeLog
+```
+
+The MyModelChangeLog class is a trivial implementation class for the abstract ChangeLog base class.
+
+Now in your client code you can activate ChangeLoggerMixin logging with two simple calls:
+
+```python
+def do_something(request):
+	# Create an instance just to have some initial data...
+	my_model = MyModel()
+	my_model.favorite_color = 'blue'
+	my_model.favorite_number = 3
+	my_model.save()
+	
+	# Now activate change tracking
+	my_model.track_changes()
+	
+	# Make changes
+	my_model.favorite_color = 'purple'
+	my_model.favorite_number = 9
+	my_model.save()
+	
+	# Log the changes
+	my_model.log_changes(request.user)
+```
+
+The design assumes a specific Django User is driving these changes and expects the User to be passed in.
+
+You'll see two new MyModelChangeLog entries:
+
+```
+2012-10-16 22:20:19+00:00 | User 1001 | MyModel 5 | favorite_color: blue->purple
+2012-10-16 22:20:19+00:00 | User 1001 | MyModel 5 | favorite_number: 3->7
+```
+
+### Customization ###
+Let's say your model has a field that always changes but you don't want to track it. No prob:
+
+```python
+class MyModel(models.Model, ChangeLoggerMixin):
+	favorite_color = models.CharField(max_length=128)
+	favorite_number = models.IntegerField()
+	last_update = models.DateTimeField()
+	
+    # Overrides from ChangeLoggerMixin
+    _change_logger_mixin__ignore_list = ['last_update',]
+    _change_logger_mixin__change_log_class = MyModelChangeLog
+```
+
+Now any changes to the last_update field will not be logged. 
--- microdev/models.py
+++ microdev/models.py
@@ -6,7 +6,7 @@ from django_extensions.db.fields import CreationDateTimeField
 
 class ChangeLogManager(models.Manager):
     
-    def log_change(self, user, obj_id, field_name, original_value, updated_value):
+    def log_change(self, user, model, obj_id, field_name, original_value, updated_value):
         if len(str(original_value)) > ChangeLog.VALUE_MAX_LENGTH:
             original_value = str(original_value)[:(ChangeLog.VALUE_MAX_LENGTH-3)] + '...'
 
@@ -14,14 +14,14 @@ class ChangeLogManager(models.Manager):
             updated_value = str(updated_value)[:(ChangeLog.VALUE_MAX_LENGTH-3)] + '...'
 
         # Create the new log entry
-        self.get_query_set().create(user=user, obj_id=obj_id, field_name=field_name, original_value=str(original_value), updated_value=str(updated_value))
+        self.get_query_set().create(user=user, model=model, obj_id=obj_id, field_name=field_name, original_value=str(original_value), updated_value=str(updated_value))
 
 
-    def log_if_changed(self, user, obj_id, field_name, original_value, updated_value):
+    def log_if_changed(self, user, model, obj_id, field_name, original_value, updated_value):
         if original_value == updated_value:
             return
         
-        self.log_change(user, obj_id, field_name, original_value, updated_value)
+        self.log_change(user, model, obj_id, field_name, original_value, updated_value)
 
 
 """--------------------------------------------------------------------
@@ -40,18 +40,9 @@ class ChangeLog(models.Model):
     
     objects = ChangeLogManager()
 
-    # ***** Override in implementation classes *****
-    def _get_model_name(self):
-        return 'MyModel'
-
-    
     class Meta:
         abstract = True
         ordering = ('-id',)
-    
-    def __init__(self, *args, **kwargs):
-        super(ChangeLog, self).__init__(*args, **kwargs)
-        self.model = self._get_model_name()
     
     def __unicode__(self):
         if len(self.original_value) > 32:
@@ -67,25 +58,35 @@ class ChangeLog(models.Model):
 
 
 
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
 class ChangeLoggerMixin():
     _original_state = {}
     
-    def _get_change_logger_mixin_ignore_list(self):
-        return []
+    # Override in the implementation class to exclude change tracking on listed fields
+    _change_logger_mixin__ignore_list = []
+    
+    # Must override to specify which ChangeLog implementation class to write to
+    _change_logger_mixin__change_log_class = None
+    
 
     # Begin tracking changes to the instance
     def track_changes(self):
         self._original_state = dict(self.__dict__)
-            
+    
     # Log any changes to the instance to the specified change_log_class
-    def log_changes(self, change_log_class, user):
+    def log_changes(self, user):
+        if not self._change_logger_mixin__change_log_class:
+            raise Exception('microdev.models.ChangeLoggerMixin: _change_logger_mixin__change_log_class was not defined in the implementation class %s' % self.__class__.__name__)
+            return
+        
         missing = None
         for key, orig_value in self._original_state.iteritems():
-            if key not in self._get_change_logger_mixin_ignore_list():
+            if key not in self._change_logger_mixin__ignore_list:
                 new_value = self.__dict__.get(key, missing)
                 if str(orig_value) != str(new_value):
                     print(key)
                     print(orig_value)
                     print(new_value)
-                    change_log_class.objects.log_if_changed(user, self.id, key, orig_value, new_value)
+                    self._change_logger_mixin__change_log_class.objects.log_change(user, self.__class__.__name__, self.id, key, orig_value, new_value)
                     
