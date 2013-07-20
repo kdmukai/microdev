e35066a0edc30f530d623322086d8802f39b8f0f
Keith Mukai <keith.mukai@essaytagger.com>
2013-07-20 15:27:20 -0500

    Added a trivial EmailForm.

--- microdev/forms.py
+++ microdev/forms.py
@@ -7,3 +7,10 @@ from django.utils.translation import gettext
 --------------------------------------------------------------------------"""
 class ConfirmCheckboxForm(forms.Form):
     confirm = forms.BooleanField(required=True, label=gettext('Confirm checkbox'))
+
+
+"""--------------------------------------------------------------------------
+    A trivial Form to handle simple email entry pages. DRY, right?
+--------------------------------------------------------------------------"""
+class EmailForm(forms.Form):
+    email = forms.EmailField(required=True)
