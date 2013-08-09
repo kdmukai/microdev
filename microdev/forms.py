fb374dbc6e29eba3a632a057a9e17371624e3c7e
Keith Mukai <keith.mukai@essaytagger.com>
2013-08-09 17:35:09 -0500

    Added basic PasswordForm.

--- microdev/forms.py
+++ microdev/forms.py
@@ -1,16 +1,35 @@
 from django import forms
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 
 
 """--------------------------------------------------------------------------
     A trivial Form to handle simple checkbox confirmation pages. DRY, right?
 --------------------------------------------------------------------------"""
 class ConfirmCheckboxForm(forms.Form):
-    confirm = forms.BooleanField(required=True, label=gettext('Confirm checkbox'))
+    confirm = forms.BooleanField(required=True, label=_('Confirm checkbox'))
 
 
 """--------------------------------------------------------------------------
     A trivial Form to handle simple email entry pages. DRY, right?
 --------------------------------------------------------------------------"""
 class EmailForm(forms.Form):
-    email = forms.EmailField(required=True)
+	email = forms.EmailField(required=True)
+
+
+"""--------------------------------------------------------------------------
+    A trivial Form to handle simple password entry pages. DRY, right?
+--------------------------------------------------------------------------"""
+class PasswordForm(forms.Form):
+	password = forms.CharField(widget=forms.PasswordInput(), required=True)
+	verify_password = forms.CharField(widget=forms.PasswordInput(), required=True)
+
+	def clean(self):
+		cleaned_data = super(PasswordForm, self).clean()
+		password = cleaned_data.get("password")
+		verify_password = cleaned_data.get("verify_password")
+
+		if password != verify_password:
+			raise forms.ValidationError(_("Verify password did not match."))
+
+		# Always return the full collection of cleaned data.
+		return cleaned_data
