df29ee45597f5f931bacdcba75c6a9c120bb1fc6
Keith Mukai <keith.mukai@essaytagger.com>
2013-08-27 13:11:01 -0700

    Further refinements to CSV import and supporting upload form.

--- microdev/csv/models.py
+++ microdev/csv/models.py
@@ -1,12 +1,37 @@
 import json
+import pickle
 
 from django.db import models
+from django.contrib import admin
 
 from django_extensions.db.fields import CreationDateTimeField
 
 
 class CsvImport(models.Model):
 	date_created = CreationDateTimeField()
+	_column_headers = models.TextField(db_column="column_headers", null=True)
+	notes = models.TextField(null=True)
+
+	def get_column_headers(self):
+		if self._column_headers:
+			return pickle.loads(self._column_headers)
+		else:
+			return None
+
+	def set_column_headers(self, value):
+		if not value:
+			self._column_headers = None
+		else:
+			self._column_headers = pickle.dumps(value)
+
+	column_headers = property(get_column_headers, set_column_headers)
+
+class CsvImportAdmin(admin.ModelAdmin):
+	readonly_fields = ('date_created',)
+
+	class Meta:
+		model = CsvImport
+
 
 class CsvImportRow(models.Model):
 	csv_import = models.ForeignKey(CsvImport)
