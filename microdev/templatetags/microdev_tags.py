e99b90441500e0df7b81bbf93a476c83c9d0eb4c
Keith Mukai <keith.mukai@essaytagger.com>
2013-09-01 01:31:47 -0700

    New templatetag to display form errors based on a reusable template.

--- microdev/templatetags/microdev_tags.py
+++ microdev/templatetags/microdev_tags.py
@@ -1,7 +1,20 @@
 from django import template
 
+from microdev.utils import render_template
+
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def show_errors(form):
+    """
+        Displays form errors using the template specified.
+    """
+    context_dict = {
+        'form': form,
+    }
+    return render_template("microdev/show_errors.html", context_dict)
