4e7c6ed66bc3efda99bc118ea055c3776d067523
Keith Mukai <keith.mukai@essaytagger.com>
2013-11-25 13:27:46 -0600

    Added ChangeLoggerModel to automatically track and log all changes. May be slightly incompatible with previous versions.

--- microdev/models.py
+++ microdev/models.py
@@ -18,13 +18,6 @@ class ChangeLogManager(models.Manager):
 		self.get_query_set().create(user=user, model=model, obj_id=obj_id, field_name=field_name, original_value=str(original_value), updated_value=str(updated_value))
 
 
-	def log_if_changed(self, user, model, obj_id, field_name, original_value, updated_value):
-		if original_value == updated_value:
-			return
-		
-		self.log_change(user, model, obj_id, field_name, original_value, updated_value)
-
-
 """--------------------------------------------------------------------
 	Abstract 
 --------------------------------------------------------------------"""
@@ -32,7 +25,7 @@ class ChangeLog(models.Model):
 	VALUE_MAX_LENGTH = 1024
 	
 	date_created = CreationDateTimeField()
-	user = models.ForeignKey(User)
+	user = models.ForeignKey(User, blank=True, null=True)
 	model = models.CharField(max_length=128)
 	obj_id = models.IntegerField()
 	field_name = models.CharField(max_length=128)
@@ -43,20 +36,12 @@ class ChangeLog(models.Model):
 
 	class Meta:
 		abstract = True
-		ordering = ('-id',)
-	
-	def __unicode__(self):
-		if len(self.original_value) > 32:
-			_orig_value = self.original_value[:32] + '...'
-		else:
-			_orig_value = self.original_value
-			
-		if len(self.updated_value) > 32:
-			_updated_value = self.updated_value[:32] + '...'
-		else:
-			_updated_value = self.updated_value[:32]
-		return "%s | User %i | %s %i | %s: %s -> %s" % (self.date_created, self.user.id, self.model, self.obj_id, self.field_name, _orig_value, _updated_value)
+		ordering = ('-pk',)
 
+class ChangeLogAdmin(admin.ModelAdmin):
+	# inlines = [VideoRecipientInline]
+	list_display = ('date_created',	'model', 'obj_id', 'field_name', 'original_value', 'updated_value',)
+	list_filter = ('date_created', 'model', 'obj_id',)
 
 
 """--------------------------------------------------------------------
@@ -77,7 +62,6 @@ class ChangeLoggerMixin():
 	def track_changes(self):
 		self._original_state = dict(self.__dict__)
 	
-	
 	# Check model's status - name is extended to avoid namespace conflicts
 	def change_logger_mixin__is_changed(self):
 		if not self._change_logger_mixin__change_log_class:
@@ -94,17 +78,41 @@ class ChangeLoggerMixin():
 
 					
 	# Log any changes to the instance to the specified change_log_class
-	def log_changes(self, user):
+	def log_changes(self, user=None):
+		from django.db import transaction
 		if not self._change_logger_mixin__change_log_class:
 			raise Exception('microdev.models.ChangeLoggerMixin: _change_logger_mixin__change_log_class was not defined in the implementation class %s' % self.__class__.__name__)
 			return
 		
 		missing = None
-		for key, orig_value in self._original_state.iteritems():
-			if key not in self._change_logger_mixin__ignore_list and key != '_original_state':
-				new_value = self.__dict__.get(key, missing)
-				if str(orig_value) != str(new_value):
-					self._change_logger_mixin__change_log_class.objects.log_change(user, self.__class__.__name__, self.id, key, orig_value, new_value)
+		with transaction.commit_on_success():	# Batches up all the log entry saves
+			for key, orig_value in self._original_state.iteritems():
+				if key not in self._change_logger_mixin__ignore_list and key != '_original_state':
+					new_value = self.__dict__.get(key, missing)
+					if str(orig_value) != str(new_value):
+						self._change_logger_mixin__change_log_class.objects.log_change(user, self.__class__.__name__, self.id, key, orig_value, new_value)
+
+
+
+class ChangeLoggedModel(models.Model, ChangeLoggerMixin):
+	""" 
+		A newer, more convenient approach that automatically logs all changes to
+		existing objects. 
+	"""
+	class Meta:
+		abstract = True
+
+	def __init__(self, *args, **kwargs):
+		super(ChangeLoggedModel, self).__init__(*args, **kwargs)
+		if self.pk:
+			# Only track changes after initial save
+			self.track_changes()
+
+	def save(self, *args, **kwargs):
+		if self.pk:
+			self.log_changes()
+		super(ChangeLoggedModel, self).save(*args, **kwargs)
+
 
 
 class UuidModel(models.Model):
@@ -117,11 +125,16 @@ class UuidModel(models.Model):
 	class Meta:
 		abstract = True
 
-	def save(self):
+	def generate_uuid(self):
+		""" Can be explictly called at any time and will be automatically called on save
+			if uuid is None """
+		import uuid
+		self.uuid = uuid.uuid4()
+
+	def save(self, *args, **kwargs):
 		if not self.uuid:
-			import uuid
-			self.uuid = uuid.uuid4()
-		super(UuidModel, self).save()
+			self.generate_uuid()
+		super(UuidModel, self).save(*args, **kwargs)
 
 class UuidModelAdmin(admin.ModelAdmin):
 	readonly_fields = ('uuid',)
@@ -138,10 +151,10 @@ class ShortUuidModel(models.Model):
 	class Meta:
 		abstract = True
 
-	def save(self):
+	def save(self, *args, **kwargs):
 		if not self.uuid:
 			self.uuid = shortuuid.uuid()
-		super(ShortUuidModel, self).save()
+		super(ShortUuidModel, self).save(*args, **kwargs)
 
 class ShortUuidModelAdmin(admin.ModelAdmin):
 	readonly_fields = ('uuid',)
