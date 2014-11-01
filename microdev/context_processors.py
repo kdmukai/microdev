f783369cae272e6152378549028668f31a67e612
kdmukai <kdmukai@kdm-mba-2014.attlocal.net>
2014-11-01 09:06:04 -0500

    Added context processor to extract the current app version num specified in app.yaml.

--- microdev/context_processors.py
+++ microdev/context_processors.py
@@ -8,8 +8,8 @@ def inject_current_appengine_version_num(request):
 	try:
 		current_version_id = os.environ['CURRENT_VERSION_ID']
 		current_appengine_version = current_version_id.split('.', 1)[0]
-		logger.debug("os.environ['CURRENT_VERSION_ID']: %s" % os.environ['CURRENT_VERSION_ID'])
-		logger.debug("current_appengine_version: %s" % current_appengine_version)
+		# logger.debug("os.environ['CURRENT_VERSION_ID']: %s" % os.environ['CURRENT_VERSION_ID'])
+		# logger.debug("current_appengine_version: %s" % current_appengine_version)
 	except AttributeError:
 		current_appengine_version = None
 
