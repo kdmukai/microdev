351a9016e2a9c4a339df047040ba7c74fcf8de9e
kdmukai <kdmukai@kdmukai.local>
2014-11-12 12:18:09 -0600

    - Added convenience function to generate named email addresses. - Minor comments.

--- microdev/utils.py
+++ microdev/utils.py
@@ -3,36 +3,58 @@ default_logger = logging.getLogger(__name__)
 
 
 def md5_hash_encode(cleartext):
-    from Crypto.Hash import MD5
-    return MD5.new(cleartext).hexdigest()
+	"""
+		MD5 any arbitrary input. Returns the 16-digit hexidecimal hash as a 32-char string.
+		e.g. md5_hash_encode("Hello, world!") = '6cd3556deb0da54bca060b4c39479839'
+	"""
+	from Crypto.Hash import MD5
+	return MD5.new(cleartext).hexdigest()
 
 
 
 """--------------------------------------------------------------------------
-    Convenience method to easily access Django's templating engine.
+	Convenience method to easily access Django's templating engine.
 --------------------------------------------------------------------------"""
 def render_template(template_name, context_dict):
-    from django.template.loader import get_template
-    from django.template import Context
+	from django.template.loader import get_template
+	from django.template import Context
 
-    # Use Django's template system to render the template
-    context = Context(context_dict)
-    
-    # Render and return as a string...
-    return get_template(template_name).render(context)
+	# Use Django's template system to render the template
+	context = Context(context_dict)
+	
+	# Render and return as a string...
+	return get_template(template_name).render(context)
 
 
 def dump_post(request, logger=None):
-    """
-        Just spits out every POST key and value in the request
-        Specify optional logger to control output.
-    """
-    for key in request.POST.keys():
-        output_str = "%s: %s" % (key, request.POST[key])
-        if logger:
-            logger.debug(output_str)
-        else:
-            default_logger.debug(output_str)
+	"""
+		Just spits out every POST key and value in the request
+		Specify optional logger to control output.
+	"""
+	for key in request.POST.keys():
+		output_str = "%s: %s" % (key, request.POST[key])
+		if logger:
+			logger.debug(output_str)
+		else:
+			default_logger.debug(output_str)
+
+
+def format_email_addressee(user):
+	"""
+		Takes an auth.User and outputs named email format "Jim Smith <jimsmith@blah.com>"
+		when possible. Otherwise just returns email.
+
+		Raw email instead of named format gets the attention of spam filters.
+	"""
+	if user.first_name and user.last_name:
+		return "%s %s <%s>" % (user.first_name, user.last_name, user.email)
+
+	elif user.first_name:
+		return "%s %s <%s>" % (user.first_name, user.last_name, user.email)
+
+	else:
+		return user.email
+
 
 
 def dump_post_str(request):
@@ -46,26 +68,26 @@ def dump_post_str(request):
 
 
 """--------------------------------------------------------------------------
-    Access Google's free currency exchange API.
+	Access Google's free currency exchange API.
 
-    Use the 3-char ISO currency codes to specify source and target currency.
-    
-    Usage:
-    Convert $15.43USD to Euros:
-    amt_in_euros = 15.43 * get_current_exchange_rate('USD', 'EUR')
+	Use the 3-char ISO currency codes to specify source and target currency.
+	
+	Usage:
+	Convert $15.43USD to Euros:
+	amt_in_euros = 15.43 * get_current_exchange_rate('USD', 'EUR')
 --------------------------------------------------------------------------"""
 def get_current_exchange_rate(src_currency, tgt_currency):
-    import urllib2
+	import urllib2
 
-    response = urllib2.urlopen('http://www.google.com/ig/calculator?hl=en&q=1%s=?%s' % (src_currency, tgt_currency))
-    data = response.read()
-    
-    # Returns hash: {lhs: "1 Euro",rhs: "1.3118 U.S. dollars",error: "",icc: true}
-    print(data)
+	response = urllib2.urlopen('http://www.google.com/ig/calculator?hl=en&q=1%s=?%s' % (src_currency, tgt_currency))
+	data = response.read()
+	
+	# Returns hash: {lhs: "1 Euro",rhs: "1.3118 U.S. dollars",error: "",icc: true}
+	print(data)
 
-    index = data.find('rhs: "') + 6
-    working_str = data[index:]
-    print(working_str)
-    exchange_rate, discard_remainder = working_str.split(' ', 1)
-    print(exchange_rate)
-    return float(exchange_rate)
+	index = data.find('rhs: "') + 6
+	working_str = data[index:]
+	print(working_str)
+	exchange_rate, discard_remainder = working_str.split(' ', 1)
+	print(exchange_rate)
+	return float(exchange_rate)
