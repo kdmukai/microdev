d8032bb08cf03b14143fd25ca5d9db11679ff81a
kdmukai <kdmukai@kdmukai.local>
2015-10-23 11:57:50 +0300

    Now deployed on pypi.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="microdev",
+    version="0.0.5",
+    author="Keith Mukai",
+    author_email="keith.mukai@essaytagger.com",
+    description=("A collection of simple reusable Django utility modules."),
+    license="BSD",
+    keywords="example documentation tutorial",
+    url="https://github.com/kdmukai/microdev",
+    packages=['microdev'],
+    # long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
