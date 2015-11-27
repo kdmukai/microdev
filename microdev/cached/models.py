13dcee59ae58d4f8ad783bda697bf73915a64366
kdmukai <kdmukai@kdmukai.local>
2015-11-27 11:29:37 -0600

    Update for CachingModel to support local memcache dev while retaining GAE-specific logic.

--- microdev/cached/models.py
+++ microdev/cached/models.py
@@ -2,23 +2,35 @@ from django.db import models
 
 from caching.base import CachingManager, CachingMixin
 
-
-"""
-	Google App Engine version to allow for script support when there's no memcache
-	environment running.
-"""
 import os
+
+try:
+	import memcache
+	memcache_available = True
+except ImportError:
+	memcache_available = False
+
 if os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine') or os.environ.get('SERVER_SOFTWARE', '').startswith('Development'):
+	is_appengine = True
+else:
+	is_appengine = False
+
+
+if is_appengine or (not is_appengine and memcache_available):
+	"""
+		Google App Engine version to allow for script support when there's no memcache
+		environment running.
+	"""
 	class CachedModel(models.Model, CachingMixin):
 		objects = CachingManager()
 
 		class Meta:
 			abstract = True
+
 else:
 	class CachedModel(models.Model):
 		""" 
-			Uncomment this version to enable shell execution when there is
-			no memcache environment.
+			Disables model caching when there is no memcache environment.
 		"""
 		class Meta:
 			abstract = True
