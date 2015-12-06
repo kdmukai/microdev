cd4ce89ee882389b07784a0fa29230b95730ff80
kdmukai <kdmukai@kdmukai.local>
2015-12-06 11:11:05 -0600

    Update to properly include templates dir.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 # Utility function to read the README file.
@@ -11,15 +11,15 @@ def read(fname):
 
 setup(
     name="microdev",
-    version="0.0.7",
+    version="0.0.8",
     author="Keith Mukai",
     author_email="keith.mukai@essaytagger.com",
     description=("A collection of simple reusable Django utility modules."),
     license="BSD",
     keywords="example documentation tutorial",
     url="https://github.com/kdmukai/microdev",
-    packages=['microdev', 'microdev.cached', 'microdev.csv', 'microdev.google_client_api', 'microdev.migrations', 'microdev.postal_address', 'microdev.templatetags'],
-    package_data={'templates':['*'],},
+    packages=find_packages(),
+    include_package_data=True,
     # long_description=read('README.md'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
